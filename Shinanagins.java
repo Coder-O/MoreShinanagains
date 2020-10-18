729caff69fd8dd6d19cd2660dd5c633a47e3fc80
Owen <owemarco@gmail.com>
2020-10-18 15:26:39 -0700

    Getting used to git.

---
+++ Shinanagins.java
@@ -0,0 +1,17 @@
+public class Shinanagins {
+
+    public static void main(String args[]){
+            System.out.println("Pinapples");
+            Limelight limelight = new Limelight();
+            limelight.gettx();
+    }
+
+
+
+
+}
+
+///////////////////////////////////////////////////////
+//       WE WANT FRIED CHICKEN.       //
+//////////////////////////////////////////////////////
+
